6a33adc7cd35b7ca30dd2a58455a55ce0dc0d9dd
s0169537 <sanyapridava@mail.ru>
2026-05-31 19:44:49 +0300

    feat complete 1.sh

--- bash/1.sh
+++ bash/1.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
+duration=${1:-60}
+interval=30     
+end_time=$((SECONDS + duration))
 
+while [ $SECONDS -lt $end_time ]; do
+    current_datetime=$(date +"%d.%m %H:%M")
+    year=$(date +%Y)
+    source="kubsu.tyvik.ru"
+    load=$(cat /proc/loadavg)
+    
+    echo "[$current_datetime]($source).$year = $load"
+    
+    if [ $SECONDS -lt $end_time ]; then
+        sleep $interval
+    fi
+done
