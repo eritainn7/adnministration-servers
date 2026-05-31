8985d8eb91c54a116c5427cbb74b82c8cf046b6c
s0169537 <sanyapridava@mail.ru>
2026-05-31 20:19:09 +0300

    feat complete 3.sh

--- bash/3.sh
+++ bash/3.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-
+for file in "$@"; do
+    if [ -f "$file" ] && [ -r "$file" ]; then
+        lines=$(wc -l < "$file")
+        echo "$file: $lines"
+    elif [ ! -e "$file" ]; then
+        echo "Ошибка: Файл '$file' не существует" >&2
+    else
+        echo "Ошибка: '$file' не является файлом или нет прав на чтение" >&2
+    fi
+done
