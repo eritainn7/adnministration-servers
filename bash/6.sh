cd44aaeb947e0a77a3d4ff8501431c298ff0f9ab
s0169537 <sanyapridava@mail.ru>
2026-05-31 20:51:27 +0300

    feat complete 6.sh

--- bash/6.sh
+++ bash/6.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
+if [ "$FOO" = "5" ] && [ "$BAR" = "1" ]; then
+    echo "Запрещено: FOO=5 и BAR=1" >&2
+    exit 1
+fi
 
+if [ $# -ne 1 ]; then
+    echo "Использование: $0 <имя_файла>"
+    exit 1
+fi
+
+while [ ! -e "$1" ]; do
+    sleep 1
+done
+
+echo "Файл $1 появился в $(pwd)"
